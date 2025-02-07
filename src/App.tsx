d53468cc63b0a5aae282aa68a3c60ca63a3fb9f0
Ben Seward <ben.seward@googlemail.com>
2025-02-07 09:45:15 +0000

    fix: fix for capitalisation issue in deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { KeyboardControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/environment";
+import { Experience } from "./components/experience";
 import { DialogBox } from "./components/dialog-box";
 import { SettingsControls } from "./components/settings-controls";
 
