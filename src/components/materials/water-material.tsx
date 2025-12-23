900f8a908ddf70d815a1b26cd3b8abee45a9b8e2
Ben Seward <ben.seward@googlemail.com>
2025-12-23 12:44:16 +0000

    test: checking removing multiply works

--- src/components/materials/water-material.tsx
+++ src/components/materials/water-material.tsx
@@ -181,8 +181,7 @@ export class WaterMaterial extends THREE.MeshStandardMaterial {
           vec4 waterColor = mix(uDepthDeepColor, uDepthShallowColor, depthDiff);
           vec4 finalColor = waterColor + vec4(surfaceNoise) + vec4(ringColor, 1.0);
 
-          gl_FragColor = vec4(1.0, 1.0, 1.0, gl_FragColor.a);
-
+          gl_FragColor = vec4(finalColor.rgb, gl_FragColor.a);
         `
       );
     };
